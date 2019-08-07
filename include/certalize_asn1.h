1eb79067cd6af41080355320473308b3d60ff917
Alexander Koeppe <format_c@online.de>
2019-08-07 13:43:07 +0200

    implement ASN1 header parsing

--- include/certalize_asn1.h
+++ include/certalize_asn1.h
@@ -20,7 +20,56 @@
 #define CERTALIZE_ASN1_H
 
 #include <certalize.h>
+#include <certalize_buf.h>
 
+#define ASN1_TAG_EOC                0x00
+#define ASN1_TAG_BOOLEAN            0x01
+#define ASN1_TAG_INTEGER            0x02
+#define ASN1_TAG_BITSTRING          0x03
+#define ASN1_TAG_OCTETSTRING        0x04
+#define ASN1_TAG_NULL               0x05
+#define ASN1_TAG_OID                0x06
+#define ASN1_TAG_OBJECT_DESCRIPTOR  0x07
+#define ASN1_TAG_EXTERNAL           0x08
+#define ASN1_TAG_REAL               0x09
+#define ASN1_TAG_ENUMERATED         0x0A
+#define ASN1_TAG_UTF8STRING         0x0C
+#define ASN1_TAG_RELATIVE_OID       0x0D
+#define ASN1_TAG_SEQUENCE           0x10
+#define ASN1_TAG_SET                0x11
+#define ASN1_TAG_NUMERIC_STRING     0x12
+#define ASN1_TAG_PRINTABLE_STRING   0x13
+#define ASN1_TAG_TG1_STRING         0x14
+#define ASN1_TAG_VIDEO_STRING       0x15
+#define ASN1_TAG_IA5_STRING         0x16
+#define ASN1_TAG_UTC_TIME           0x17
+#define ASN1_TAG_GERNERALIZED_TIME  0x18
+#define ASN1_TAG_GRAPHIC_STRING     0x19
+#define ASN1_TAG_VISIBLE_STRING     0x1A
+#define ASN1_TAG_GENERAL_STRING     0x1B
+#define ASN1_TAG_UNIVERSAL_STRING   0x1C
+#define ASN1_TAG_BMP_STRING         0x1D
+
+#define ASN1_CLASS_UNIVERSAL        0
+#define ASN1_CLASS_APPLICATION      1
+#define ASN1_CLASS_CONTEXT_SPECIFIC 2
+#define ASN1_CLASS_PRIVATE          3
+
+typedef struct asn1_hdr {
+   guint8 class;
+   guint8 constructed;
+   guint32 tag;
+   guint8 length_byte;
+   guint32 length;
+} asn1_hdr_t;
+
+#define ASN1_MAX_OID_LEN            20
+typedef struct asn1_oid {
+   guint64 oid[ASN1_MAX_OID_LEN];
+   size_t len;
+} asn1_oid_t;
+
+extern int asn1_parse_hdr(cbuf_t *cbuf, asn1_hdr_t *hdr);
 
 
 #endif   /* CERTALIZE_ASN1_H */
--- src/asn1.c
+++ src/asn1.c
@@ -30,6 +30,72 @@
 /*************/
 
 
+int asn1_parse_hdr(cbuf_t *cbuf, asn1_hdr_t *asn1)
+{
+   int offset = cbuf->offset;
+   guchar buf, *ptr, tmp;
+
+   memset(asn1, 0, sizeof(asn1_hdr_t));
+
+   ptr = &buf;
+
+   /* get 1st byte of ASN.1 header */
+   cbuf_get_bytes(cbuf, &ptr, offset++, 1);
+
+   asn1->class = buf >> 6; // first MSB's of ID are the CLASS
+   asn1->constructed = (buf & (1<<5)); // constructed if 6th bit is set
+
+   if ((buf & 0x1f) == 0x1f) {
+      // High Tag if all 5 LSB's are set
+      asn1->tag = 0;
+
+      cbuf_get_bytes(cbuf, &ptr, offset++, 1);
+
+      do {
+         // append the 7 LSB's to the tag value
+         // TODO prevent against integer overflow
+         asn1->tag = (asn1->tag << 7) | (buf & 0x7f);
+
+         tmp = buf;
+         if (buf & 0x80) { // still one more byte for the tag left
+            cbuf_get_bytes(cbuf, &ptr, offset++, 1);
+         }
+
+      } while (tmp & 0x80);
+   }
+   else {
+      // tag is just the 5 LSB's of the ID
+      asn1->tag = buf & 0x1f;
+   }
+   
+
+   /* 
+    * parsing tag byte(s) complete, 
+    * now proceed with the length byte(s)
+    */
+   cbuf_get_bytes(cbuf, &ptr, offset++, 1);
+   asn1->length_byte = buf;
+   
+   if (asn1->length_byte & 0x80) {
+      tmp = asn1->length_byte & 0x7f;
+      while (tmp--) {
+         cbuf_get_bytes(cbuf, &ptr, offset++, 1);
+         // TODO avoid integer overflow
+         asn1->length = (asn1->length << 8) | buf;
+      }
+   }
+   else {
+      asn1->length = asn1->length_byte & 0x7f;
+   }
+
+
+   /*
+    * offset is now the amount of bytes processed for the ASN.1 header
+    */
+   return offset;
+
+}
+
 /* EOF */
 
 // vim:ts=3:expandtab
